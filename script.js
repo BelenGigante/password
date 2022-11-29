0ae0dfe3c32197b223863c201f921bc7c4913cfc
BelenGigante <vinn_a@hotmail.com>
2022-11-29 17:54:07 -0500

    fixed alert errors

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-var isUppercase = comfirm("use uppercase");
-var isLowercase = comfirm("use lowercase");
-var isSpecial = comfirm("use special characters");
-var isNumber = comfirm("use numbers");
+function generatePassword() {
+  var possibleSpecial = "!#$%&()*+,-./:;<=>?@[^_`{|}~";
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var upletters = letters.toUpperCase();
+  var isUppercase = confirm('use uppercase');
+  var isLowercase = confirm("use lowercase");
+  var isSpecial = confirm("use special characters");
+  var isNumber = confirm("use numbers");
 
-if (isUppercase){
-  console.log("uppercase");
-}
-if (isLowercase){
-  console.log("lowercase");
-}
-if (isSpecial){
-  console.log("special");
-}
-if (isNumber){
-  console.log("number");
-}
-return "";
+  if (isUppercase) {
+    console.log("uppercase");
+  }
+  if (isLowercase) {
+    console.log("lowercase");
+  }
+  if (isSpecial) {
+    console.log("special");
+  }
+  if (isNumber) {
+    console.log("number");
+  }
+  return "";
 }
 // Write password to the #password input
-/*function writePassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}*/
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
