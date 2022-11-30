9b6666ab088a498cd0f57ae03d8f6f5b28235cb2
BelenGigante <vinn_a@hotmail.com>
2022-11-29 20:18:53 -0500

    array test

--- script.js
+++ script.js
@@ -9,33 +9,33 @@ function generatePassword() {
   var isLowercase = confirm("use lowercase?");
   var isSpecial = confirm("use special characters?");
   var isNumber = confirm("use numbers?");
-  var passwordLength = 10;
-  var outcome= "";
-  var password = "";
+  var password = {
+    outcome:["","","",""],
+  };
 
   if (isUppercase){
-    outcome= abcs.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
-    console.log(outcome.toUpperCase());
+    password.outcome[0]= abcs.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
+    console.log(password.outcome[0].toUpperCase());
 
   }else{
     
   }
   if (isLowercase) {
-    outcome= abcs.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
+    password.outcome[1]= abcs.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
   
-    console.log(outcome);
+    console.log(password.outcome[1]);
   }else{
 
   }
   if (isSpecial) {
-    outcome= special.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
-    console.log(outcome);
+    password.outcome[2]= special.substring(Math.floor(Math.random() * numbers.length),Math.floor(Math.random() * numbers.length));
+    console.log(password.outcome[2]);
   }else{
 
   }
   if (isNumber) {
-    var number = Math.floor(Math.random() * abcs.length);
-    console.log(number);
+    password.outcome[3] = Math.floor(Math.random() * abcs.length);
+    console.log(password.outcome[3]);
   }else{
 
   }
@@ -44,11 +44,11 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
+ //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
